3049c05f0ae0a4d3fe015afcf0dce40d26012133
saifulhaxan <saifulhaxan@gmail.com>
2025-01-02 05:17:41 +0500

    Auth Completed

--- src/Screens/Auth/ForgetPassword3.js
+++ src/Screens/Auth/ForgetPassword3.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation, Link } from 'react-router-dom';
 
 
 
@@ -9,39 +9,79 @@ import CustomButton from '../../Components/CustomButton';
 import CustomModal from '../../Components/CustomModal';
 
 import "./style.css";
+import { useAuth } from '../../Api';
+import { toast } from 'react-toastify';
 
 const ForgetPassword3 = () => {
     const navigate = useNavigate()
-
+    const location = useLocation();
     const [showModal, setShowModal] = useState(false);
+    const queryParams = new URLSearchParams(location.search);
+    const paramValue = queryParams.get("token");
+    const [formData, setFormData] = useState({
+        token: paramValue
+    })
+
+    const { ApiData: ChangePasswordResponse, loading: ChangePasswordLoadning, error: ChangePasswordError, post: submitData } = useAuth('auth/reset-password');
 
     useEffect(() => {
         document.title = 'Poker City | Password Recovery';
     }, [])
 
 
-    const handleClick = () => {
-        setShowModal(true)
+    const handleClick = (e) => {
+        e.preventDefault();
+        if (formData?.token && formData?.password && formData?.confirmPassword) {
+            submitData(formData);
+        }
+
     }
 
-    const redirectHome = () => {
-        navigate('/dashboard')
-    }
+    useEffect(() => {
+        if (ChangePasswordResponse) {
+            toast(ChangePasswordResponse?.message);
+            setTimeout(() => {
+                navigate('/login')
+            }, 1500)
+        }
+    }, [ChangePasswordResponse])
+
+
+    useEffect(() => {
+        if (ChangePasswordError) {
+            toast(ChangePasswordError?.message);
+        }
+    }, [ChangePasswordError])
+
+
+
+
+
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFormData((prevData) => ({
+            ...prevData,
+            [name]: value,
+        }));
+        console.log('formData', formData)
+    };
+
 
     return (
         <>
             <AuthLayout authTitle='Password Recovery' authPara='Enter a new password.' backOption={true}>
-                <form>
-                    <CustomInput label='New Password' required id='pass' type='password' placeholder='Enter New Password' labelClass='mainLabel' inputClass='mainInput' />
-                    <CustomInput label='Confirm Password' required id='cPass' type='password' placeholder='Confirm Password' labelClass='mainLabel' inputClass='mainInput' />
+                <form onSubmit={handleClick}>
+                    <CustomInput label='New Password' required id='pass' type='password' name="password" placeholder='Enter New Password' labelClass='mainLabel' inputClass='mainInput' onChange={handleChange} />
+                    <CustomInput label='Confirm Password' required id='cPass' type='password' name="confirmPassword" placeholder='Confirm Password' labelClass='mainLabel' inputClass='mainInput' onChange={handleChange} />
 
                     <div className="mt-4 text-center">
-                        <CustomButton type='button' variant='primaryButton' text='Update' onClick={handleClick} />
+                        <CustomButton type='submit' variant='primaryButton' text='Update' />
                     </div>
                 </form>
             </AuthLayout>
 
-            <CustomModal show={showModal} success heading='Password updated successfully. Please login to continue' close={redirectHome} btnTxt="Continue"/>
+            {/* <CustomModal show={showModal} success heading='Password updated successfully. Please login to continue' btnTxt="Continue" /> */}
         </>
     )
 }
